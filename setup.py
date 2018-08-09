1121afdeb6a5ba6829d67f8d7a75fa29602081c2
Matteo Guadrini <m.guadrini@gmail.com>
2018-08-09 09:40:08 +0200

    Add setup.py file; Add setup instructions to README and docs

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Butterfly-Backup',
+    version='1.0.0',
+    packages=[''],
+    url='https://matteoguadrini.github.io/Butterfly-Backup/',
+    license='GNU General Public License v3.0',
+    author='Matteo Guadrini',
+    author_email='matteo.guadrini@hotmail.it',
+    description='Butterfly Backup is a simple command line wrapper of rsync for complex task, written in python.'
+)
